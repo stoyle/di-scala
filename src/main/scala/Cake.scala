e080c56a8378143e01f8a4a3be28e695cd904da5
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-03-18 23:05:21 +0100

    Showing explicit use of self name.

--- src/main/scala/Cake.scala
+++ src/main/scala/Cake.scala
@@ -25,7 +25,7 @@ trait UserServiceComponent {
     def authenticate(username: String, password: String): User =   
       userRepository.authenticate(new User(username, password))    
     def create(username: String, password: String) =   
-      userRepository.create(new User(username, password))  
+      self.userRepository.create(new User(username, password))  
     def delete(user: User) = userRepository.delete(user)  
   }  
 }
