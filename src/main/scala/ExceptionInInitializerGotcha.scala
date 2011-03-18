7f2389b8ddaee591b6d39eb6cfccf8026a599ea1
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-03-18 17:49:41 +0100

    A more output on the ExceptionInInitializerError issue.

--- src/main/scala/ExceptionInInitializerGotcha.scala
+++ src/main/scala/ExceptionInInitializerGotcha.scala
@@ -13,7 +13,10 @@ object Main extends Application {
     }
     catch {
         case e: ExceptionInInitializerError => {
-            println("When instantiated as part of an object, the root cause is what we are after: " + e.getCause)
+            println("When instantiated as part of an object, the root cause is what want: " + e.getCause)
+            println("getMessage() is useless really: " + e.getMessage)
+            println("printStackTrace() is a little bit better, but with uneccessary nesting:")
+            e.printStackTrace
         }
     }
     
@@ -24,7 +27,10 @@ object Main extends Application {
     }
     catch {
         case e: RuntimeException => {
-            println("When instantiated as a part of a class, we get the real exception: " + e)
+            println("When instantiated as a part of a class, we get the real exception straight away : " + e)
+            println("getMessage() is useful: " + e.getMessage())
+            println("printStackTrace() does not wrap anything: ")
+            e.printStackTrace
         }
     }
     
