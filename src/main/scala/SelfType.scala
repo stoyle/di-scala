11ae8d97b2b864f4b80741024d64d03c4a6562a7
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-03-19 15:00:18 +0200

    A little more examples of selftypes.

--- src/main/scala/SelfType.scala
+++ src/main/scala/SelfType.scala
@@ -1,29 +1,42 @@
 package SelfType
 
 trait A {
-    def foo: Unit = println("In A.foo")
-    override def toString = "A"
+  def foo: Unit = println("In A.foo")
 }
 
 trait B {
-    self: A =>
-    def bar: Unit = {
-        println("In B.bar")
-        self.foo
-        this.foo
-    }   
+  self: A =>
+  def bar: Unit = {
+    println("In B.bar")
+    self.foo
+    this.foo
+  }   
 }
 
 trait C {
-    self: B =>
-    def baz: Unit = {
-        // Does not compile
-        // self.foo
-        // this.foo
+  self: B =>
+  def baz: Unit = {
+    // Does not compile
+    // self.foo
+    // this.foo
 
-        println("In C.baz")
-        bar
-    }   
+    println("In C.baz")
+    bar
+  }   
+}
+
+trait D {
+  selfName: A with B =>
+  
+  def callSelfTypes {
+    foo
+    this.foo
+    selfName.foo
+    bar
+    selfName.bar
+    this.bar
+  }
+  
 }
 
 object Main extends C with B with A with Application {
